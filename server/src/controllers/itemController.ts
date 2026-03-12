f59f5e4c6fe2c840cb767ac28eecd8ca7ce0ec36
Moshe Siegel <siegel.moshes@gmail.com>
2026-03-12 10:49:54 +0200

    Server - proper error handling for updating and deleting an item

--- server/src/controllers/itemController.ts
+++ server/src/controllers/itemController.ts
@@ -31,18 +31,28 @@ export const updateItem = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id as string);
   const { name } = req.body;
 
-  const item = await itemRepository.updateItem(id, name);
-
-  if (!item) throw createError(404, 'Item not found');
-  res.json({ success: true, data: { item } });
+  try {
+    const item = await itemRepository.updateItem(id, name);
+    res.json({ success: true, data: { item } });
+  } catch (error) {
+    if ((error as any).code === 'P2025') {
+      throw createError(404, 'Item not found');
+    }
+    throw error;
+  }
 };
 
 // Delete an item
 export const deleteItem = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id as string);
 
-  const deletedItem = await itemRepository.deleteById(id);
-
-  if (!deletedItem) throw createError(404, 'Item not found');
-  res.json({ success: true, data: { item: deletedItem } });
+  try {
+    const deletedItem = await itemRepository.deleteById(id);
+    res.json({ success: true, data: { item: deletedItem } });
+  } catch (error) {
+    if ((error as any).code === 'P2025') {
+      throw createError(404, 'Item not found');
+    }
+    throw error;
+  }
 };
